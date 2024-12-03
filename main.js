1f131bf7db36e018302fd596a9020625cade8031
AndreaCaro42 <andreacarolinaflorezalvarado@gmail.com>
2024-12-03 08:21:10 -0500

    Operaciones matematicas

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
-console.log "Estoy en JS"
+console.log("Estoy en JS")
 
+let $resultado = document.getElementById("Resul")
+
+function fSumar(){
+    let num1 = parseInt(n1.value)
+    let num2 = parseInt(n2.value)
+    /*console.log (typeof(num1))*/
+    $resultado.innerHTML = "La suma es: "+ (num1+num2)
+}
+function fRestar(){
+    let num1= parseInt (n1.value)
+    let num2= parseInt (n2.value)
+    $resultado.innerHTML="La resta es: "+ (num1-num2)
+}
+function fMultiplicar(){
+    let num1= parseInt (n1.value)
+    let num2= parseInt (n2.value)
+    $resultado.innerHTML = "La multiplicación es: "+ (num1*num2)
+}
+function fDividir(){
+    let num1= parseInt (n1.value)
+    let num2= parseInt (n2.value)
+    if(num2===0)
+    {$resultado.innerHTML = "La division entre cero (0) No es posible"}
+    else{$resultado.innerHTML = "La division es: "+(num1/num2)}
+}
